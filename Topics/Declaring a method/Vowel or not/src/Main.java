c350c0b178c91077f75c9259f64b639b6410abf2
davidcastellanos <castellanos.johann@gmail.com>
2022-02-07 23:58:03 -0500

    [UPDATE] Added Topics

---
+++ Topics/Declaring a method/Vowel or not/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isVowel(char ch) {
+        // write your code here
+        char c = Character.toLowerCase(ch);
+
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char letter = scanner.nextLine().charAt(0);
+        System.out.println(isVowel(letter) ? "YES" : "NO");
+    }
+}
