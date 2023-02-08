baa3267533c414ba2545b5d5e192832498ea2319
davidcastellanos <castellanos.johann@gmail.com>
2023-02-08 11:57:07 -0500

    [UPDATE] Adding new challenge

--- Topics/Iterating over arrays/Individual taxes/src/Main.java
+++ Topics/Iterating over arrays/Individual taxes/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,73 +7,33 @@ public class Main {
 
         int numOfComps = scanner.nextInt();
         int[] incomeByComp = new int[numOfComps];
-        int[] businessTax = new int[numOfComps];
-        final double hundred = 100;
-        int a = 0;
-        double taxesPaid = 0;
-        double highestPayingComp = 0;
-        int companyIndex = 1;
+        int[] taxesByComp = new int[numOfComps];
+        int paidTaxes = Integer.MIN_VALUE;
+        int companyWithMostTaxes = 0;
 
-
-        for (int i = 0; i < incomeByComp.length; i++) {
+        for (int i = 0; i < numOfComps; i++) {
             incomeByComp[i] = scanner.nextInt();
         }
 
-        for (int i = 0; i < businessTax.length; i++) {
-            businessTax[i] = scanner.nextInt();
+        for (int i = 0; i < numOfComps; i++) {
+            taxesByComp[i] = scanner.nextInt();
         }
 
-
-
-
-        for (int j = 0; j < incomeByComp.length; j++) {
-            taxesPaid = (incomeByComp[j] / hundred) * businessTax[j];
-
-            if (taxesPaid > highestPayingComp) {
-                highestPayingComp = taxesPaid;
-                companyIndex = companyIndex + j;
-            }
-            //a++;
-
-        }
-
-
-       /* if ( incomeByComp.length == 1) {
-            highestPayingComp = 1;
+        if (numOfComps == 1) {
+            companyWithMostTaxes = incomeByComp.length;
 
         } else {
-            for (int i = 0; i < incomeByComp.length - 1; i++) {
-
-                a = (incomeByComp[i] / hundred) * businessTax[i];
-                b = (incomeByComp[i + 1] / hundred) * businessTax[i + 1];
-                System.out.println(a);
-                System.out.println(b);
-
-                if (a >= b) {
-                    acumA = acumA + 1;
-                    highestPayingComp = i + 1;
-                    System.out.println("Acum A " + acumA);
-
-                } else {
-                    acumB = acumB + 1;
-                    highestPayingComp = i + 2;
-                    System.out.println("Acum B " + acumB);
-
+            for (int i = 1; i <= incomeByComp.length; i++) {
+                int tempTaxes = incomeByComp[i - 1] * taxesByComp[i - 1];
+                if (paidTaxes < tempTaxes) {
+                    paidTaxes = tempTaxes;
+                    companyWithMostTaxes = i;
                 }
-
-                //highestPayingComp = acumA > acumB ? i + 1 : i + 2;
-
-
             }
+        }
 
-        }*/
+        System.out.println(companyWithMostTaxes);
 
 
-
-
-        /*System.out.println(Arrays.toString(incomeByComp));
-        System.out.println(Arrays.toString(businessTax));
-        System.out.println(highestPayingComp);*/
-        System.out.println(companyIndex);
     }
 }
